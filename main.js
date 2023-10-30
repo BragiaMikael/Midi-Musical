82d689c8c15f8bfb731d476c7967654901240f1b
BragiaMikael <134522614+BragiaMikael@users.noreply.github.com>
2023-10-30 20:48:43 -0300

    wee

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 alert('Olá Mundo')
-function tocaSomPom(){
+function tocaSomSabia(){
     document.querySelector('#som_tecla_sabia').play();
 }
 document.querySelector('.tecla_sabia').onclick = tocaSomSabia;
 
-function tocaSomClap(){
+function tocaSomLarangeira(){
     document.querySelector('#som_tecla_larangeira').play();
 }
 document.querySelector('.tecla_larangeira').onclick = tocaSomLarangeira
