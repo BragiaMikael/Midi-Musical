a253544c36ed6c80b0fa5c7bc32c4b588af18440
BragiaMikael <134522614+BragiaMikael@users.noreply.github.com>
2023-10-31 11:34:49 -0300

    bff

--- main.js
+++ main.js
@@ -48,3 +48,8 @@ function tocaSomTom(){
     document.querySelector('#som_tecla_tom').play();
 }
 document.querySelector('.tecla_tom').onclick = tocaSomTom
+
+function tocaSabia(){
+    document.querySelector('#som_tecla_sabia').play();
+}
+document.querySelector('.tecla_sabia').onclick = tocaSabia
