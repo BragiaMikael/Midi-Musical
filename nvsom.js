6c94444a69eed2d0b6b2acc481914235093bab0c
BragiaMikael <134522614+BragiaMikael@users.noreply.github.com>
2023-11-01 08:57:15 -0300

    adicionando som do larangeira

--- nvsom.js
+++ nvsom.js
@@ -2,3 +2,8 @@ function tocaSomSabia(){
     document.querySelector('#som_tecla_sabia').play();
 }
 document.querySelector('.tecla_sabia').onclick = tocaSomSabia
+
+function tocaSomLarangeira(){
+    document.querySelector('#som_tecla_larangeira').play();
+}
+document.querySelector('.tecla_larangeira').onclick = tocaSomLarangeira
